afde6ebf813b3113340bcaefa7dcddc6779cd01a
QuaternionWithSoul <nailzagru@gmail.com>
2024-12-05 20:07:14 +0300

    bluetooth

--- strawberry/system/services/bluetooth.nix
+++ strawberry/system/services/bluetooth.nix
@@ -1,8 +1,37 @@
 { ... }: {
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
+  hardware = {
+    enableAllFirmware = true;
+    enableRedistributableFirmware = true;
+
+    bluetooth = {
+      enable = true;
+      package = pkgs.bluez5-experimental;
+
+      powerOnBoot = true;
+
+      settings = {
+        General = {
+          Enable = "Source,Sink,Media,Socket";
+          Name = "Hello";
+          ControllerMode = "dual";
+          FastConnectable = "true";
+          Experimental = "true";
+          KernelExperimental = "true";
+        };
+
+        Policy = {
+          AutoEnable = "true";
+        };
+      };
+    };
   };
 
   services.blueman.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    bluez-experimental 
+    bluez-tools
+    bluez-alsa
+    bluetuith
+  ];
 }
