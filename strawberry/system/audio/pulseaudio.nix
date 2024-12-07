d4afbfe5e2d3df425e6e98305377684237a943ba
QuaternionWithSoul <nailzagru@gmail.com>
2024-12-07 11:06:31 +0300

    pulseaudio

--- strawberry/system/audio/pulseaudio.nix
+++ strawberry/system/audio/pulseaudio.nix
@@ -1,8 +1,25 @@
-{ ... }: {
+{ pkgs, ... }: {
   hardware.pulseaudio = {
     enable = true;
+    package = pkgs.pulseaudioFull;
+
     support32Bit = true;
+
+    configFile = pkgs.writeText "default.pa" ''
+      load-module module-bluetooth-policy
+      load-module module-bluetooth-discover
+      ## module fails to load with 
+      ##   module-bluez5-device.c: Failed to get device path from module arguments
+      ##   module.c: Failed to load module "module-bluez5-device" (argument: ""): initialization failed.
+      # load-module module-bluez5-device
+      # load-module module-bluez5-discover
+    '';
+
+    extraConfig = "
+      load-module module-combine-sink
+      load-module module-switch-on-connect
+    ";
   };
 
-  services.pipewire.enable = false;
+  nixpkgs.config.pulseaudio = true;
 }
